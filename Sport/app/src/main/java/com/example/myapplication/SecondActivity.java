8e35c50d4552ecd84dd691a406ced09484762f22
GYJ <2354684632@qq.com>
2023-11-12 22:08:09 +0800

    开始运动按钮动画v1.0，跳转开始运动活动

---
+++ Sport/app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
--- Sport/app/src/main/java/com/example/myapplication/fragment/SportFragment.java
+++ Sport/app/src/main/java/com/example/myapplication/fragment/SportFragment.java
@@ -1,18 +1,21 @@
 package com.example.myapplication.fragment;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
+import com.example.myapplication.SecondActivity;
 
 public class SportFragment extends Fragment {
 
@@ -30,15 +33,53 @@ public class SportFragment extends Fragment {
                 }
         );*/
         View mView = inflater.inflate(R.layout.fragment_sport, container, false);
-        Button button = mView.findViewById(R.id.ButtonGo);
+
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Button button = view.findViewById(R.id.ButtonGo);
         button.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         Log.d("BUTTON_TEST","press go");
+                        button.setText("");
+                        button.setBackgroundResource(R.drawable.animation_go);//把Drawable设置为button的背景
+                        //拿到这个我们定义的Drawable，实际也就是AnimationDrawable
+                        AnimationDrawable animationDrawable = (AnimationDrawable) button.getBackground();
+                        animationDrawable.setOneShot(true);// 播放一次
+                        animationDrawable.start();//开启动画
+                        Log.d("ANI_TEST","end playing");
+                        //Button jumpFragment = view.findViewById(R.id.start_sport_fragment);
+
+                        // 需要另外启动一个线程 在新线程里面去sleep
+                        // 不要在UI线程中sleep UI被sleep会暂停刷新
+                        Thread t = new Thread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                try {
+                                    Thread.sleep(3000);
+                                } catch (InterruptedException e) {
+                                    // TODO Auto-generated catch block
+                                    e.printStackTrace();
+                                }
+                                Intent intent = new Intent(getActivity(),SecondActivity.class);
+
+                                intent.putExtra("mydata", "来自页面一");
+                                startActivity(intent);
+                            }
+                        });
+
+                        // 开启线程
+                        t.start();
+
+
+
                     }
                 }
         );
-        return mView;
     }
 }
