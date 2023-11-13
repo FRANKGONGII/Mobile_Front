3551337a9bdb6166a1360a0e7a51a1d6020770a1
GYJ <2354684632@qq.com>
2023-11-13 16:07:50 +0800

    开始运动按钮动画v2,增加种类选择按钮

--- Sport/app/src/main/java/com/example/myapplication/RecordingActivity.java
+++ Sport/app/src/main/java/com/example/myapplication/RecordingActivity.java
@@ -3,15 +3,14 @@ package com.example.myapplication;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
 
-public class SecondActivity extends AppCompatActivity {
+public class RecordingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_recording);
+        //TODO: 接入地图，完成运动记录相关功能
+
     }
 }
--- Sport/app/src/main/java/com/example/myapplication/fragment/SportFragment.java
+++ Sport/app/src/main/java/com/example/myapplication/fragment/SportFragment.java
@@ -1,13 +1,18 @@
 package com.example.myapplication.fragment;
 
+import static java.lang.Thread.sleep;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,71 +20,130 @@ import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
-import com.example.myapplication.SecondActivity;
+import com.example.myapplication.RecordingActivity;
 
 public class SportFragment extends Fragment {
+
+    String sport_type = "running";//默认运动是跑步
+    private AlertDialog alertDialog2;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        /*Button button = getActivity().findViewById(R.id.ButtonGo);
-        button.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Log.d("BUTTON_TEST","press go");
-                    }
-                }
-        );*/
         View mView = inflater.inflate(R.layout.fragment_sport, container, false);
+
+        //TODO：这里应该要去获取上面面板的数据
+
 
         return mView;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        setButtonGO(view);
+        setButtonChoose(view);
+    }
+
+    public void setButtonGO(@NonNull View view) {
         Button button = view.findViewById(R.id.ButtonGo);
         button.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         Log.d("BUTTON_TEST","press go");
-                        button.setText("");
-                        button.setBackgroundResource(R.drawable.animation_go);//把Drawable设置为button的背景
-                        //拿到这个我们定义的Drawable，实际也就是AnimationDrawable
-                        AnimationDrawable animationDrawable = (AnimationDrawable) button.getBackground();
-                        animationDrawable.setOneShot(true);// 播放一次
-                        animationDrawable.start();//开启动画
-                        Log.d("ANI_TEST","end playing");
+
+                        final int[] cntTime = {3};
+                        Handler handler = new Handler();
+                        Runnable runnable = new Runnable(){
+                            @Override
+                            public void run(){
+                                //数字自增
+                                // 创建文本
+                                if(cntTime[0]>0)button.setText(String.valueOf(cntTime[0]));
+                                else if(cntTime[0]==0) button.setText("GO!");
+                                cntTime[0]--;
+                                handler.postDelayed(this, 1000);
+                            }
+                        };
+                        handler.post(runnable);
+
+//                        button.setBackgroundResource(R.drawable.animation_go);//把Drawable设置为button的背景
+//                        //拿到这个我们定义的Drawable，实际也就是AnimationDrawable
+//                        AnimationDrawable animationDrawable = (AnimationDrawable) button.getBackground();
+//                        animationDrawable.setOneShot(true);// 播放一次
+//                        animationDrawable.start();//开启动画
+//
+//                        Log.d("ANI_TEST","end playing");
                         //Button jumpFragment = view.findViewById(R.id.start_sport_fragment);
 
                         // 需要另外启动一个线程 在新线程里面去sleep
                         // 不要在UI线程中sleep UI被sleep会暂停刷新
                         Thread t = new Thread(new Runnable() {
-
                             @Override
                             public void run() {
                                 try {
-                                    Thread.sleep(3000);
+                                    sleep(3200);
+                                    //注意，这里设置成3500是为了让GO！能显示一下
                                 } catch (InterruptedException e) {
                                     // TODO Auto-generated catch block
                                     e.printStackTrace();
                                 }
-                                Intent intent = new Intent(getActivity(),SecondActivity.class);
+                                Intent intent = new Intent(getActivity(), RecordingActivity.class);
 
-                                intent.putExtra("mydata", "来自页面一");
+                                intent.putExtra("mydata", "来自main");
                                 startActivity(intent);
                             }
                         });
-
                         // 开启线程
                         t.start();
-
-
-
                     }
                 }
         );
     }
+
+    public void setButtonChoose(@NonNull View view){
+        Button button = view.findViewById(R.id.ButtonChooseType);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSingleAlertDialog(view);
+            }
+        });
+    }
+
+    public void showSingleAlertDialog(View view){
+        final String[] items = {"跑步", "骑行", "游泳", "快走"};
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getActivity());
+        alertBuilder.setTitle("这是单选框");
+
+        final int[] choose = {-1};
+        alertBuilder.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getActivity(), items[i], Toast.LENGTH_SHORT).show();
+                Log.d("CHOOSE_TEST","CHOOSE:"+String.valueOf(i));
+                choose[0] =i;
+            }
+        });
+
+        alertBuilder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Log.d("CHOOSE_TEST",String.valueOf(choose[0]));
+
+                alertDialog2.dismiss();
+            }
+        });
+
+        alertBuilder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                alertDialog2.dismiss();
+            }
+        });
+        alertDialog2 = alertBuilder.create();
+        alertDialog2.show();
+    }
+
 }
