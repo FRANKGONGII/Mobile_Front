73e23397dd38d4ecc537066441cf38a3da3744ac
GYJ <2354684632@qq.com>
2023-11-14 12:45:09 +0800

    地图引入尝试v1

--- Sport/app/src/main/java/com/example/myapplication/RecordingActivity.java
+++ Sport/app/src/main/java/com/example/myapplication/RecordingActivity.java
@@ -4,13 +4,30 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RecordingActivity extends AppCompatActivity {
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.LocationSource;
+import com.amap.api.maps2d.MapView;
 
+
+public class RecordingActivity extends AppCompatActivity {
+    private MapView mapView;
+    private AMap aMap;
+    private LocationSource.OnLocationChangedListener mListener = null;//定位监听器
+    //private LocationUtil locationUtil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recording);
         //TODO: 接入地图，完成运动记录相关功能
 
+
+        mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        //init();
+
+
     }
+
+
+
 }
