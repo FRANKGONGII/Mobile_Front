ff94048ca104a6fb93ed88423fea344af5a5275a
DIVINLYF <2756978878@qq.com>
2023-11-18 20:42:56 +0800

    主页界面框架

--- Sport/app/src/main/java/com/example/myapplication/fragment/HomeFragment.java
+++ Sport/app/src/main/java/com/example/myapplication/fragment/HomeFragment.java
@@ -1,20 +1,112 @@
 package com.example.myapplication.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeFragment extends Fragment {
+
+    public static final String TAG = "HomeFragment";
+    private DrawerLayout drawerLayout;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home,container, false);
+
+        //Toolbar toolbar = view.findViewById(R.id.toolbar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        //activity.setSupportActionBar(toolbar);
+        //activity.getSupportActionBar().setTitle("Top Bar");
+
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        toolbar.setTitle("TOP");
+
+
+        setHasOptionsMenu(true);
+
+        drawerLayout = view.findViewById(R.id.drawer_layout);
+        if (drawerLayout == null) activity.getSupportActionBar().setTitle("OOPS");
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.baseline_home_24);
+        }
+
+        //响应菜单项点击事件
+        NavigationView navView = view.findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                //后续添加处理逻辑
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
+        //设置背景图,更换背景图
+//        ImageView imageView = view.findViewById(R.id.userBkgImg);
+//        imageView.setImageResource(R.drawable.user_bkg_test);
+
+
+        //填充页面
+        TextView textView = view.findViewById(R.id.test_text);
+        String txt = "Banana~";
+        for (int i = 0; i < 10; ++i) txt += txt;
+        textView.setText(txt);
+
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.toolbar, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            drawerLayout.openDrawer(GravityCompat.START);
+        }
+        return true;
     }
 }
