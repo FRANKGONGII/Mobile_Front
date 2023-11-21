1b95fa3ae9a05d57066f1260a3d177a4af3a5ada
DIVINLYF <2756978878@qq.com>
2023-11-21 15:08:19 +0800

    merge

--- Sport/app/src/main/java/com/example/myapplication/User/User.java
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication.User;
-
-public class User {
-
-
-}
---
+++ Sport/app/src/main/java/com/example/myapplication/User/UserAccount.java
@@ -0,0 +1,9 @@
+package com.example.myapplication.User;
+
+import java.io.Serializable;
+
+public class UserAccount implements Serializable {
+
+
+
+}
