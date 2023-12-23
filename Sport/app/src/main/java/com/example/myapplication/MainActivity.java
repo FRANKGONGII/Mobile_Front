33044434073d64d69a23bb8de5afa59cf8106df4
DIVINLYF <2756978878@qq.com>
2023-12-23 17:04:40 +0800

    改变底部导航栏

--- Sport/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Sport/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,29 +1,49 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+
+import github.com.st235.lib_expandablebottombar.ExpandableBottomBar;
+import github.com.st235.lib_expandablebottombar.navigation.ExpandableBottomBarNavigationUI;
+
 public class MainActivity extends AppCompatActivity {
 
-    BottomNavigationView bottomNavigation;
+    private ViewPager viewPager;
+    private ArrayList<Fragment> fragments;
+//    BottomNavigationView bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigation = findViewById(R.id.bottom_navigation);
+//        viewPager = findViewById(R.id.viewPager);
+//        bottomNavigation = findViewById(R.id.bottom_navigation);
 
         //获取navController
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         //通过setupWithNavController将底部导航和导航控制器进行绑定
-        NavigationUI.setupWithNavController(bottomNavigation,navController);
+//        NavigationUI.setupWithNavController(bottomNavigation,navController);
+
+        ExpandableBottomBar expandableBottomBar = findViewById(R.id.expandable_bottom_bar);
+
+        ExpandableBottomBarNavigationUI.setupWithNavController(expandableBottomBar, navController);
+
+
+
+
 
 
     }
